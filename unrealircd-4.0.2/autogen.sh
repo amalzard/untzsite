d4ea82a7ef6f3a7592243b91207aebe3c99a6e9f
amalzard <amalzard@hotmail.co.uk>
2017-04-07 00:39:50 -0700

    removed un needed files

--- unrealircd-4.0.2/autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "${0}")"
-
-ACLOCAL_AMFLAGS=(-I autoconf/m4)
-
-aclocal "${ACLOCAL_AMFLAGS[@]}"
-autoconf
-autoheader
--- unrealircd-4.0.2/src/win32/encpem.bat
+++
@@ -1,3 +0,0 @@
-@title Encrypting server private key
-openssl rsa -in conf/ssl/server.key.pem -out conf/ssl/server.key.c.pem -des3
-copy conf/ssl/server.key.c.pem conf/ssl/server.key.pem
