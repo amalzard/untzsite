1e332f85823fd0add691672efe76cb7da0b5f0b7
amalzard <amalzard@hotmail.co.uk>
2017-04-11 10:09:00 -0700

    General clean-up of main page. Uneeded scripts removed plus comments added.

--- index.php
+++ index.php
@@ -5,8 +5,6 @@ session_start();
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="css/default.css" title="default">
-<link rel="alternate stylesheet" type="text/css" href="css/megumin.css" title="megumin">
-<link rel="alternate stylesheet" type="text/css" href="css/totori.css" title="totori">
 
 <meta charset="UTF-8">
 <title>Welcome to UNTZ!</title>
@@ -16,98 +14,72 @@ session_start();
 </head>
 <body>
 
-<div class='page'>
-
 <div class="title">
 <img src="sitelogo.png" height="52" width="52" />
 <img src="logo.png"/>
 </div>
-</div>
 
-
-
-
-<div class='video'>
-<div id="myElement">Loading the player...</div>
-<script type="text/javascript">
-var playerInstance = jwplayer("myElement");
-playerInstance.setup({
+<div class='page'>
+	<div class='video'>
+		<div id="myElement">Loading the player...</div>
+		<script type="text/javascript">	//Script to initilise and load the video player.
+		var playerInstance = jwplayer("myElement");
+		playerInstance.setup({
 	
 
-sources: [{
-	    file: "rtmp://untz.xen.prgmr.com/live/flv:untz.flv"
-	},{
-	    file: "http:///untz.xen.prgmr.com/live/untz/index.m3u8"
-	},{
-	    file: "http://untz.xen.prgmr.com/live/untz/index.mp4"
-	}],
-	rtmp: {
+		sources: [{
+	    		file: "rtmp://untz.xen.prgmr.com/live/flv:untz.flv"
+			},{
+	    		file: "http:///untz.xen.prgmr.com/live/untz/index.m3u8"
+			},{
+	   		 file: "http://untz.xen.prgmr.com/live/untz/index.mp4"
+			}],
+			rtmp: {
 
-            bufferlength: 3
-        },
-            fallback: "false",
-	    androidhls: "true",
-	    width: "100%",
-	    image: "./myPoster.jpg",
-	    autostart: "true",
-	    primary: "flash",
-	    aspectratio: "16:9"
-
-
-});
-</script>
-</div>
-
-<div class='statContainer'>
-<div id="stat">
-<div class='statsBackground streamStatsGrey' id='viewcount' style="font-family:untzFont;"><span id='viewcount'></span></div>
-<div class='statsBackground streamStatsGrey' id='current' style="font-family:untzFont;"><span id='current'></span></div>
-</div>
+            		bufferlength: 3
+        		},
+           		fallback: "false",
+	   		androidhls: "true",
+	   		width: "100%",
+	    		image: "./myPoster.jpg",
+	    		autostart: "true",
+	    		primary: "flash",
+	    		aspectratio: "16:9"
 
 
-<div class="reminder">
-	<div align= 'center' class='PopOut buttonGrey unselectable' onclick="popChat()">
-		<span class='PopOut'>Pop-out Chat</span>
+			});
+		</script>
 	</div>
-</div>
+
+	<div class='statContainer'>
+		<div id="stat">
+			<div class='statsBackground streamStatsGrey' id='viewcount' style="font-family:untzFont;"><span id='viewcount'></span></div>
+			<div class='statsBackground streamStatsGrey' id='current' style="font-family:untzFont;"><span id='current'></span></div>
+		</div>
+	</div>
+
+
+	<div class="reminder">
+		<div align= 'center' class='PopOut buttonGrey unselectable' onclick="popChat()">
+			<span class='PopOut'>Pop-out Chat</span>
+		</div>
+	</div>
 	<div align= 'center' class='PopOut buttonGrey unselectable' id='click'>
 		<span class='PopOut'>Show/Hide Chat Sidebar</span>
 	</div>
 </div>
 
-<div id="footer">
-
 <div class="info" style="font-family:untzFont;">
-<h1><br/><br/> Welcome to UNTZTIME live broadcast! </h1>
-Only LIVE DJ mixing with hardcore, j-core, house and EDM weeb tunes straight out of Japan.<br>
-<h2> Like us? </h2>
-Join our Discord community to chat with fellow untzers:<br>
-https://discord.gg/0ha2CfyN2PXNiE9H<br>
-Follow us on Twitter for anime streams, gaming streams, events and more:<br>
-https://twitter.com/UntzTime<br>
-Join our steam group and plan some vidya sessions:<br>
-http://steamcommunity.com/groups/untztime<br>
-<br>
-<br>
-</div>
-
-<br>
-<br>
-
-
-<div class="drunek">
-<img src="drunek.png" height="560" />
-</div>
-
-<form>
-<select>
-  <option onclick="switch_style('default');return false;" id="default">default</option>
-  <option onclick="switch_style('megumin');return false;" id="megumin">megumin</option>
-  <option onclick="switch_style('totori');return false;" id="totori">totori</option>
-</select> 
-</form>
-
-</div>
+	<h1><br/><br/> Welcome to UNTZTIME live broadcast! </h1>
+	Only LIVE DJ mixing with hardcore, j-core, house and EDM weeb tunes straight out of Japan.<br>
+	<h2> Like us? </h2>
+	Join our Discord community to chat with fellow untzers:<br>
+	https://discord.gg/0ha2CfyN2PXNiE9H<br>
+	Follow us on Twitter for anime streams, gaming streams, events and more:<br>
+	https://twitter.com/UntzTime<br>
+	Join our steam group and plan some vidya sessions:<br>
+	http://steamcommunity.com/groups/untztime<br>
+	<br>
 </div>
 
 <div id='sidebar'>
@@ -117,31 +89,13 @@ http://steamcommunity.com/groups/untztime<br>
 </body>
 </html>
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-74405756-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
 
-<script type="text/javascript">
-       setInterval(refreshIframe1, 10000);
-       function refreshIframe1() {
-           $("#stats")[0].src = $("#stats")[0].src;
-       }
-
-   </script>
 <script>
 function myFunction() {
-if(document.getElementById("panel").style.display == "none") {
-    document.getElementById("panel").style.display = "block";
-} else {document.getElementById("panel").style.display = "none"}
+	if(document.getElementById("panel").style.display == "none") {
+    		document.getElementById("panel").style.display = "block";
+	} else {document.getElementById("panel").style.display = "none"}
 }
 </script>
 
@@ -156,7 +110,7 @@ $(function() {
  
 });
  
-function getStatus() {
+function getStatus() {		//function to update current number of viewers and current streamer.
  
     $('span#current').load('displayUntzStreamer.php');
     $('span#viewcount').load('untzviews.php');
@@ -179,82 +133,25 @@ function popChat() {
 
 <script>
 
-$('#click').click(function()
+$('#click').click(function()	//function that changes margin properties of content on sidebar toggle.
 {   
 	$("#sidebar").toggle();
 	if ( $('#sidebar').is(':hidden') ) {
 		$('.video').css('margin-left', '250px');
 		$('.video').css('margin-right', '250px');
+		$('.info').css('margin-left', '250px');
+		$('.info').css('margin-right', '250px');
 		$('.statContainer').css('margin-left', '250px');
-	}else 
-		{$('.video').css('margin-right', '500px');
+	}else {
+		$('.video').css('margin-right', '500px');
 		$('.video').css('margin-left', '0px');
+		$('.info').css('margin-right', '500px');
+		$('.info').css('margin-left', '0px');
 		$('.statContainer').css('margin-left', '0px');
 	}
 
 		
 });
-
-</script>
-
-<script>
-
-var style_cookie_name = "style" ;
-var style_cookie_duration = 30 ;
-var style_domain = "untz.xen.prgmr.com" ;
-
-
-function switch_style ( css_title )
-{
-// You may use this script on your site free of charge provided
-// you do not remove this notice or the URL below. Script from
-// http://www.thesitewizard.com/javascripts/change-style-sheets.shtml
-  var i, link_tag ;
-  for (i = 0, link_tag = document.getElementsByTagName("link") ;
-    i < link_tag.length ; i++ ) {
-    if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) &&
-      link_tag[i].title) {
-      link_tag[i].disabled = true ;
-      if (link_tag[i].title == css_title) {
-        link_tag[i].disabled = false ;
-      }
-    }
-    set_cookie( style_cookie_name, css_title,
-      style_cookie_duration, style_domain );
-  }
-}
-function set_style_from_cookie()
-{
-  var css_title = get_cookie( style_cookie_name );
-  if (css_title.length) {
-    switch_style( css_title );
-  }
-}
-function set_cookie ( cookie_name, cookie_value,
-    lifespan_in_days, valid_domain )
-{
-    // http://www.thesitewizard.com/javascripts/cookies.shtml
-    var domain_string = valid_domain ?
-                       ("; domain=" + valid_domain) : '' ;
-    document.cookie = cookie_name +
-                       "=" + encodeURIComponent( cookie_value ) +
-                       "; max-age=" + 60 * 60 *
-                       24 * lifespan_in_days +
-                       "; path=/" + domain_string ;
-}
-function get_cookie ( cookie_name )
-{
-    // http://www.thesitewizard.com/javascripts/cookies.shtml
-    var cookie_string = document.cookie ;
-    if (cookie_string.length != 0) {
-        var cookie_value = cookie_string.match (
-                        '(^|;)[\s]*' +
-                        cookie_name +
-                        '=([^;]*)' );
-        return decodeURIComponent ( cookie_value[2] ) ;
-    }
-    return '' ;
-}
 
 </script>
 
